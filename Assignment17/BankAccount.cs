9775b7e0de466aeca75699e2bf37575ca073e43a
prakash1926 <ananthaprakash71@gmail.com>
2023-11-21 18:24:46 +0530

    First commit

---
+++ Assignment17/BankAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17
+{
+    public class BankAccount
+    {
+        readonly long AccountNumber;
+        string AccountHolderName;
+        double Balance;
+
+        public BankAccount(long AccountNumber, string AccountHolderName, double Balance)
+        {
+            this.AccountNumber = AccountNumber;
+            this.AccountHolderName = AccountHolderName;
+            this.Balance = Balance;
+        }
+        public long Account { get { return AccountNumber; } }
+        public string Name { get { return AccountHolderName; } }
+        public double Bal { get { return Balance; } }
+
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+            Console.WriteLine($"Deposited Amount {amount:C}\n Updated balance: {Balance:C}\n");
+        }
+
+        public void Withdraw(double amount)
+        {
+
+                Balance -= amount;
+                Console.WriteLine($"Withdrawn {amount:C}\n Updated balance: {Balance:C}\n");
+            
+        }
+
+        public void PrintAccountDetails()
+        {
+            Console.WriteLine($"Account Number: {AccountNumber}");
+            Console.WriteLine($"Account Holder: {AccountHolderName}");
+            Console.WriteLine($"Available Balance: {Balance:C}");
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Assignment17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(5656,"Ananth",1000);
+
+            account.Deposit(1000);
+            account.Withdraw(500);
+
+            account.PrintAccountDetails();
+
+            Console.ReadKey();
+        }
+    
+    }
+}
